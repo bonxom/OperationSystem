f70eba5e975ef55081cc720d5d90eab5e776308a
Ho Minh Dung <bonxom@gmail.com>
2025-03-23 01:10:26 +0700

    Update input organization

--- tinyShell.c
+++ tinyShell.c
@@ -1,6 +1,10 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <unistd.h>
+#include <sched.h>
+#include <signal.h>
+#include <string.h>
+#include <ctype.h>
 
 // ASCII Art for TinyShell
 const char *tinyShellArt[] = {
@@ -19,8 +23,6 @@ const char *tinyShellArt[] = {
 
 };
 
-int isForegroundMode = 0;
-
 void printAnimatedText() {
     int numLines = sizeof(tinyShellArt) / sizeof(tinyShellArt[0]);
     
@@ -32,6 +34,7 @@ void printAnimatedText() {
 
 void help(){
     printf("\ntinyShell supports the following commands:\n\n");
+    printf("clear       : Clear screen\n");
     printf("child       : Run the child process\n");
     printf("list        : List all background processes\n");
     //kill -> completely stop
@@ -52,19 +55,7 @@ void help(){
     printf("\n");
 }
 
-void kill(int type, char *id){ //type 0: PID, type 1: name
-
-}
-
-void stop(int type, char *id){
-
-}
-
-void resume(int type, char *id){
-
-}
-
-int main() {
+void introduction(){
     system("clear");
     printf("\n\n");
     printAnimatedText();
@@ -77,6 +68,48 @@ int main() {
     printf("\tHo Minh Dung 20235050\n");
     printf("\n_________________________________________________________________________________\n");
     help();
+}
+
+void kil(int type, char *id){ //type 1: PID, type 0: name
+
+}
+
+void stop(int type, char *id){
+
+}
+
+void resume(int type, char *id){
+
+}
+
+void list(){
+    
+}
+
+void dir(){
+
+}
+
+void date(){
+
+}
+
+void time_(){
+
+}
+
+
+int isNumeric(char *str) {
+    if (str == NULL || *str == '\0') return 0; // Check for NULL or empty string
+
+    for (int i = 0; i < strlen(str); i++) {
+        if (!isdigit(str[i])) return 0; // If any character is not a digit, return false
+    }
+    return 1; // All characters are digits
+}
+
+int main() {
+    introduction();
 
     while (1){
         printf("ShellHehe> ");
@@ -86,7 +119,46 @@ int main() {
 
         if (strcmp(s, "exit") == 0) break;
         else if (strcmp(s, "help") == 0) help();
-        else printf("\nUndefined command, try again!\n\n");
+        else if (strcmp(s, "dir") == 0) dir();
+        else if (strcmp(s, "date") == 0) date();
+        else if (strcmp(s, "time") == 0) time_();
+        else if (strcmp(s, "child") == 0) {
+            pid_t pid = fork();
+            if (pid == 0) {
+                printf("Child process is running\n");
+                exit(0);
+            }
+        }
+        else if (strcmp(s, "list") == 0) {
+            printf("List of background processes:\n");
+            list();
+        }
+        else if (strcmp(s, "clear") == 0) system("clear");
+        else{//kill, stop, resume
+            //input should split by space, example kill 1234, so we need to split kill and 1234
+            //then check if 1234 is a number or not, if it is a number, then it is a PID, otherwise, it is a name
+            char **str = malloc(4 * sizeof(char *));
+            int i = 0;
+            str[i] = strtok(s, " ");
+            while (str[i] != NULL) {
+                i++;
+                if (i >= 4) break; // Prevent buffer overflow
+                str[i] = strtok(NULL, " ");
+            }
+
+            if (i != 2) {
+                printf("Invalid command, try again!\n");
+                continue;
+            }
+            if (str[0] != NULL && str[1] != NULL) {
+                int type = isNumeric(str[1]);
+                if (strcmp(str[0], "kill") == 0) kil(type, str[1]);
+                else if (strcmp(str[0], "resume") == 0) resume(type, str[1]);
+                else if (strcmp(str[0], "stop") == 0) stop(type, str[1]);
+                else printf("Invalid command, try again!\n");
+            }
+
+        }
     }
     return 0;
 }
