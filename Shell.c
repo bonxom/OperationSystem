1b342665ad2cd9af8f9b00be86e36c731c4da71c
Nguyen Quang Duc <Duc.NQ235044@sis.hust.edu.vn>
2025-03-22 18:20:41 +0000

    Demo

--- Shell.c
+++ Shell.c
@@ -1,5 +1,92 @@
 #include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+
+// ASCII Art for TinyShell
+const char *tinyShellArt[] = {
+    "___________________________________________________________________________________",
+    "|                                                                                 |",
+    "|     ████████╗██╗███╗   ██╗██╗   ██╗███████╗██╗  ██╗███████╗██╗     ██╗          |",
+    "|     ╚══██╔══╝██║████╗  ██║██║   ██║██╔════╝██║  ██║██╔════╝██║     ██║          |",
+    "|        ██║   ██║██╔██╗ ██║██║   ██║███████╗███████║█████╗  ██║     ██║          |",
+    "|        ██║   ██║██║╚██╗██║██║   ██║╚════██║██╔══██║██╔══╝  ██║     ██║          |",
+    "|        ██║   ██║██║ ╚████║╚██  ██╔╝███████║██║  ██║███████╗███████╗███████╗     |",
+    "|        ╚═╝   ╚═╝╚═╝  ╚═══╝ ╚████╔╝ ╚══════╝╚═╝  ╚═╝╚══════╝╚══════╝╚══════╝     |",
+    "|                              ██╔╝                                               |",
+    "|                              ██║                                                |",
+    "|                              ╚═╝                                                |",
+    "|_________________________________________________________________________________|",
+
+};
+
+int isForegroundMode = 0;
+
+void printAnimatedText() {
+    int numLines = sizeof(tinyShellArt) / sizeof(tinyShellArt[0]);
+    
+    for (int i = 0; i < numLines; i++) {
+        printf("%s\n", tinyShellArt[i]);
+        usleep(150000); // Sleep for 150 milliseconds to create animation effect
+    }
+}
+
+void help(){
+    printf("\ntinyShell supports the following commands:\n\n");
+    printf("child       : Run the child process\n");
+    printf("list        : List all background processes\n");
+    //kill -> completely stop
+    printf("kill <PID>  : Kill the process with the given PID\n");
+    printf("kill <name> : Kill the process with the given name\n");
+    //stop -> temporary stop
+    printf("stop <PID>  : Stop the process with the given PID\n");
+    printf("stop <name> : Stop the process with the given name\n");
+    //resume-> continue a process (which was stopped)
+    printf("resume <PID>: Resume a stopped process with the given PID\n");
+    printf("resume<name>: Resume a stopped process with the given name\n");
+
+    printf("dir         : List the contents of the current directory\n");
+    printf("date        : Display the system date\n");
+    printf("time        : Display the system time\n");
+    printf("exit        : Exit my shell\n");
+    printf("help        : Print this help\n");
+    printf("\n");
+}
+
+void kill(int type, char *id){ //type 0: PID, type 1: name
+
+}
+
+void stop(int type, char *id){
+
+}
+
+void resume(int type, char *id){
+
+}
+
 int main() {
-    printf("Hello, Tiny Shell!\n");
+    system("clear");
+    printf("\n\n");
+    printAnimatedText();
+    printf("\nWelcome to TinyShell!\n");
+    printf("\nThis shell is programmed for Unix as a project for the Operating Systems course at HUST.\nTeam members:\n");
+    printf("\tNguyen Quang Duc 20235044\n");
+    printf("\tHo Minh Dung 20235050\n");
+    printf("\tLe Duc Chinh 20235023\n");
+    printf("\tTran Gia Dinh 20235036\n");
+    printf("\tNguyen Ba Duc Anh 20235004\n");
+    printf("\n_________________________________________________________________________________\n");
+    help();
+
+    while (1){
+        printf("ShellHehe> ");
+        char s[1000];
+        fgets(s, 1000, stdin);
+        s[strcspn(s, "\n")] = 0;
+
+        if (strcmp(s, "exit") == 0) break;
+        else if (strcmp(s, "help") == 0) help();
+        else printf("\nUndefined command, try again!\n\n");
+    }
     return 0;
 }
